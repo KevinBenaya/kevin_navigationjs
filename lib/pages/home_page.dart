07d9b235beb2f9cd5560d6e284740c81b75adc5c
KevinBenaya <darkfaust063@gmail.com>
2022-03-22 09:46:03 +0700

    Have an error when adding Item's object

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
 
   @override
   Widget build(BuildContext context) {
