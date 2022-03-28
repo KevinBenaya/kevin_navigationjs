b3d2b132e36f5244421dd4d06b85a3cd83fc92cd
KevinBenaya <darkfaust063@gmail.com>
2022-03-28 16:09:11 +0700

    create home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:kevin_navigationjs/models/item.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   final List<Item> items = [
@@ -9,6 +11,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Card(
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(child: Text(item.name)),
+                    Expanded(
+                      child: Text(
+                        item.price.toString(),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
