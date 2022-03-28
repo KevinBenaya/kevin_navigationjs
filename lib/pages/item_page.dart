618f2714bfd3381bca027854bdd7e474ac1d9ec5
KevinBenaya <darkfaust063@gmail.com>
2022-03-28 16:27:31 +0700

    item_pages

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:kevin_navigationjs/models/item.dart';
+
 class ItemPage extends StatelessWidget {
   const ItemPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final Item itemArgs = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Text(
+              itemArgs.name,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    )
   }
 }
